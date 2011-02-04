3ed9ccb3a992885be143ca4c60a0d7e163b48544
Mathew Cave <mcave@matthewcave.com>
2011-02-04 13:41:21 -0600

    updated script

--- list_tasks.rb
+++ list_tasks.rb
@@ -1,9 +1,26 @@
+# #########################################
+#        wunderlist-display
+# 
+# @author Matthew Cave
+# @date Feb 4, 2011
+# @abstract Looks up the projects and tasks in your wunderlist.db, and
+#   exports them in a ANSI marked up manner appropriate for display in geektool3
+# ##########################################
+
 require 'sqlite3'
 
-db = SQLite3::Database.new( "/Users/mcave/Library/Application Support/Titanium/appdata/com.wunderkinder.wunderlist/wunderlist.db" )
+db = SQLite3::Database.new( "/Users/<<your username here>>/Library/Application Support/Titanium/appdata/com.wunderkinder.wunderlist/wunderlist.db" )
+
 db.results_as_hash = true # results returned as a hash, rather than an ordered array
 db.type_translation = true # results resurned as their native types (according to the column definition) rather than String
- 
+
+ESCAPE_IMPORTANT_COLOR = "\033[33m" # defaults to yellow
+ESCAPE_PAST_DUE_COLOR = "\033[31m" # defaults to red
+ESCAPE_PROJECT_NAME_MARKUP = "\033[1m" # defauls to bold
+ESCAPE_NOTE_MARKUP = "\033[3m" # defaults to italic
+ESCAPE_CANCEL = "\033[0m" # resets all text atributes you may have set
+INDENT = "     "
+
 db.execute("select id, name, position from lists where deleted = '0' order by position") do |list|
     printedHeader = false
     db.execute("select id, date, name, note, important, position from tasks where deleted = 0 and done = 0 and list_id = ? order by date DESC, position", list['id']) do |task|
@@ -11,7 +28,7 @@ db.execute("select id, name, position from lists where deleted = '0' order by po
         # There is a task associated with this list, so print the header
         if (printedHeader == false) then
             puts
-            puts "\033[1m" + list['name'] + "\033[0m"  # this prints a header
+            puts ESCAPE_PROJECT_NAME_MARKUP + list['name'] + ESCAPE_CANCEL  # this prints a header
             printedHeader = true
         end
         
@@ -24,16 +41,16 @@ db.execute("select id, name, position from lists where deleted = '0' order by po
             
             #if the date is in the past, print the date in red
             if (today > date) then 
-                taskToOutput += "     " + "\033[31m" + date.month.to_s + "/" + date.day.to_s + "/" + date.year.to_s + "\033[0m" + " - "
+                taskToOutput += INDENT + ESCAPE_PAST_DUE_COLOR + date.month.to_s + "/" + date.day.to_s + "/" + date.year.to_s + ESCAPE_CANCEL + " - "
             else
-                taskToOutput += "     " + date.month.to_s + "/" + date.day.to_s + "/" + date.year.to_s + " - "
+                taskToOutput += INDENT + date.month.to_s + "/" + date.day.to_s + "/" + date.year.to_s + " - "
             end
         else
-            taskToOutput += "     " 
+            taskToOutput += INDENT 
         end
         
         if (task['important'] == 1) then
-          taskToOutput += "\033[33m" + "★ " + "\033[0m"
+          taskToOutput += ESCAPE_IMPORTANT_COLOR + "★ " + ESCAPE_CANCEL
         end
         taskToOutput += task['name']
         
@@ -41,7 +58,7 @@ db.execute("select id, name, position from lists where deleted = '0' order by po
         
         # if there is a note, print it in italics
         if (!task['note'].nil?) then
-          puts "          " + "\033[3m" + task['note'] + "\033[0m" 
+          puts INDENT + INDENT + ESCAPE_NOTE_MARKUP + task['note'] + ESCAPE_CANCEL 
         end # end if (is there a task note)
     end # end do |task| loop
 end
